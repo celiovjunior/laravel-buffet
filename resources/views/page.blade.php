85d231007f0b1e093e0d65960ed5632c6a257a3a
Célio Júnior <celio_cjr@hotmail.com>
2023-03-05 17:32:02 -0300

    study: testing router and layout concept

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="./css/styles.css">
-  <title>Document</title>
-</head>
-<body>
-  <h1>Page</h1>
-  <script src="./js/script.js"></script>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('title', 'About us - Code Buffet')
+    @section('content')
+@endsection
+
+<a href="/">⏪</a>
+<h1>page test</h1>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./css/styles.css">
-    <title>Laravel</title>
-</head>
-
-<body class="antialiased">
-    <h1>hello world</h1>
-    <img src="./img/image-1.svg" alt="">
-    <script src="./js/script.js"></script>
-</body>
-
-</html>
+@section('title', 'Code Buffet')
+    @section('content')
+@endsection
+<h1>welcome</h1>
+<a href="/page">click here</a>
